dc59a77f185ce57db49c72f12f9c285030085889
S. M. RUHUL KABIR HOWLADER <ruhul5347@outlook.com>
2018-08-14 21:59:41 +0600

    After Entity Framework

---
+++ VidlyV6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VidlyV6.MvcApplication" Language="C#" %>
---
+++ VidlyV6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyV6.Startup))]
+namespace VidlyV6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
