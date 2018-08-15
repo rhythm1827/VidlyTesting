4d27724dfe1eb69840e47d48928d30474a44db8d
S. M. RUHUL KABIR HOWLADER <ruhul5347@outlook.com>
2018-08-15 20:35:23 +0600

    Exercise 3(3)

--- VidlyV6/Controllers/MoviesController.cs
+++ VidlyV6/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,18 +11,27 @@ namespace VidlyV6.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
+        private ApplicationDbContext _context;
+
+        public MoviesController()//ctor tab
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
             return View(movies);
         }
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
+            var movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+            return View(movie);
         }
     }
 }
--- VidlyV6/Models/IdentityModels.cs
+++ VidlyV6/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace VidlyV6.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Customer> Customers { get; set; }
-
+        public DbSet<Movie> Movies { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
